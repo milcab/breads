44910b4b922a729ef16ff1e2e527f73faba838f9
milcab <milcarodriguez23@gmail.com>
2021-12-17 23:13:24 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Welcome to Awesome App about Breads!')
+})
+const breadsController = require('.//controllers/breads_controller.js')
+app.use('/breads', breadsController)
+app.listen(PORT, () => {
+    console.log('nomming at port', PORT);
+})
